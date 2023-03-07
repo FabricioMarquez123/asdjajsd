a3c6162f1c134fc8b16b6e2cfd6e5601dd2eaa0a
FabricioMarquez123 <114788319+FabricioMarquez123@users.noreply.github.com>
2023-03-07 11:30:28 -0600

    Delete parcial 1 directory

--- parcial 1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace parcial_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
